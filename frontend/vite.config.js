6021eacb33822380c90b81e39bda32ae15ef36c0
Nikhil <nikhilgaur1610@gmail.com>
2024-07-25 13:48:20 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "https://streaming-platform-ww08.onrender.com",
 			},
 		},
 	},
